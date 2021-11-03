5021112478d5de6a1de0fc0531145d3ecb36c401
l6_pc2 <l6_pc2@WIN-IM38HI1GTD6>
2021-11-03 14:07:47 +0300

    Add project files.

---
+++ metin_islemleri/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿
+namespace metin_islemleri
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnTikla = new System.Windows.Forms.Button();
+            this.lstAlfabe = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnTikla
+            // 
+            this.btnTikla.Location = new System.Drawing.Point(318, 165);
+            this.btnTikla.Name = "btnTikla";
+            this.btnTikla.Size = new System.Drawing.Size(108, 61);
+            this.btnTikla.TabIndex = 0;
+            this.btnTikla.Text = "Tıkla";
+            this.btnTikla.UseVisualStyleBackColor = true;
+            this.btnTikla.Click += new System.EventHandler(this.btnTikla_Click);
+            // 
+            // lstAlfabe
+            // 
+            this.lstAlfabe.FormattingEnabled = true;
+            this.lstAlfabe.Location = new System.Drawing.Point(130, 101);
+            this.lstAlfabe.Name = "lstAlfabe";
+            this.lstAlfabe.Size = new System.Drawing.Size(120, 147);
+            this.lstAlfabe.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lstAlfabe);
+            this.Controls.Add(this.btnTikla);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnTikla;
+        private System.Windows.Forms.ListBox lstAlfabe;
+    }
+}
+
---
+++ metin_islemleri/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace metin_islemleri
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //metnin karakter sayısını bulma
+            
+            string meyve = "      kiraz     ";
+           MessageBox.Show( meyve.Trim());
+            int sayi = meyve.Length;
+            MessageBox.Show(sayi.ToString());
+
+            //seçilen indexleri gösterir ilki kaçıncı indis ikinci kısım kaç tane kelime olacağını belirler
+
+            MessageBox.Show(meyve.Substring(1,2));
+
+
+            
+
+           
+
+            //karakteri farklı bir karakterle değişitirir
+
+            string replaceDeger= meyve.Replace('i', 'a');
+            MessageBox.Show(replaceDeger);
+
+
+
+
+            // belli indexten başlayarak belirtilen sayı kadar karakter silme
+
+            string yeniMeyve = meyve.Remove(3, 2);
+            MessageBox.Show(yeniMeyve.ToString());
+           
+
+            // bir karakterden istenilen sayı kadar yazdırmak
+            
+
+            //string a = new string('a',6);
+            //MessageBox.Show(a);
+
+        }
+
+        private void btnTikla_Click(object sender, EventArgs e)
+        {
+            string alfabe = "A B C Ç D E F G Ğ H I İ J K L M N O Ö P R S Ş T U Ü V Y Z";
+            alfabe = alfabe.Replace(" ", "");
+            //char[] harfler = alfabe.ToCharArray();
+            string[] harfler = new string[28];
+
+            for (int i = 0; i < harfler.Length; i++)
+            {
+                harfler[i]= alfabe.Substring(i,1);
+                lstAlfabe.Items.Add(harfler[i].ToString());
+            }
+
+            
+        }
+    }
+}
